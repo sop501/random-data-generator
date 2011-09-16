ba8dd8c235c2472c541de98cda09ae287eac51af
ramon@thebuckland.com <ramon@thebuckland.com@2d0292ee-1771-9295-0582-545065a96a7e>
2011-09-16 12:43:42 +0000

    first release 0.01

---
+++ tags/release-0.01/src/main/java/org/soqqo/datagen/config/DataFieldConfig.java
@@ -0,0 +1,44 @@
+package org.soqqo.datagen.config;
+
+import java.security.SecureRandom;
+
+public class DataFieldConfig {
+
+    private int constructorPosition;
+
+    private String propertyName;
+
+    public DataFieldConfig(String propertyName, Enum generationType) {
+        this.propertyName = propertyName;
+        this.generationType = generationType;
+    }  
+
+    /**
+     * One of the DataTypes.* enums
+     */
+    private Enum generationType;
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+
+    public void setPropertyName(String propertyName) {
+        this.propertyName = propertyName;
+    }
+
+    public int getConstructorPosition() {
+        return constructorPosition;
+    }
+
+    public void setConstructorPosition(int constructorPosition) {
+        this.constructorPosition = constructorPosition;
+    }
+
+    public Enum getGenerationType() {
+        return generationType;
+    }
+
+    public void setGenerationType(Enum generationType) {
+        this.generationType = generationType;
+    }
+}
